504cfead5c261f55bb04d9f26fb8732519c64c04
inhooham90 <inhooham90@gmail.com>
2020-10-05 22:50:19 -0400

    update menu items to natigave withRouter higher order component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -51,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map( ({ title, imageUrl, id, size }) => {
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map( ({ id, ...otherProps }) => {
+                        return <MenuItem key={id} {...otherProps} />
                     })
                 }
             </div>
