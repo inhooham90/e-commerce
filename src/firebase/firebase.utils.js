dd7676b542147026941b1b6e28efad2c34e44ad3
inhooham90 <inhooham90@gmail.com>
2020-10-08 21:58:59 -0400

    add firebase user to the db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,7 +32,7 @@ export const createUserProfileDocument = async (userAuth, data) => {
         ...data
       })
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log('error creating user: ', error.message);
     }
   }
 
