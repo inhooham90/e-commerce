9148eb66c4d429f38efe5885f4815905003aa12a
inhooham90 <inhooham90@gmail.com>
2020-10-09 11:14:11 -0400

    implement sign up feature

--- src/pages/auth-page/auth-page.component.jsx
+++ src/pages/auth-page/auth-page.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import "./auth-page.style.scss";
 
 const AuthPage = () => (
-    <div className="sign-in-and-sign-up" >
+    <div className="auth" >
         <SignIn />
+        <SignUp />
     </div>
 )
 
